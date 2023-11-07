9582518332c5346eeb714d7acc658d6385c2974d
julo57 <julian.mikuskiewicz@gmail.com>
2023-11-07 20:50:21 +0100

    Logowanie działa

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import useAuthContext from "../context/AuthContext";
-import { Navigate } from "react-router-dom"; // Zaimportuj Navigate
+import { Navigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import Cookies from 'js-cookie'; // Importuj bibliotekę js-cookie
 
 const Profile = () => {
-  const { user } = useAuthContext(); // Pobierz informacje o zalogowanym użytkowniku z kontekstu autoryzacji
+  const { user, logout } = useAuthContext(); // Pobierz informacje o zalogowanym użytkowniku i funkcję wylogowywania z kontekstu autoryzacji
+  const { t } = useTranslation("global");
+  // Funkcja do wylogowywania i czyszczenia cookies
+  const handleLogout = () => {
+    logout(); // Wyloguj użytkownika
+    Cookies.remove('loginCookie'); // Usuń cookie związane z logowaniem
+  }
 
   // Jeśli użytkownik nie jest zalogowany, przekieruj go na stronę logowania
   if (!user) {
@@ -11,14 +19,22 @@ const Profile = () => {
   }
   else{
     return (
-      <div>
-        <h1>Mój Profil</h1>
-        <p>Witaj, {user.name}!</p>
-      </div>
+      <>
+        <div className="min-h-screen flex flex-col items-start justify-start ml-4" style={{position: 'relative', top: '70px', overflowX: 'hidden'}}>
+          <div className="w-full text-2xl" style={{position: 'relative', top: '1px', left: '60px', bottom: '20px'}}>
+            <h1>{t("profile.title")}, {user.name}!</h1>
+          </div>
+          <div className="w-1/4 p-4" style={{borderRight: '1px solid gray', paddingRight: '10px'}}>
+            <button className="block w-full p-3 mb-4 text-black-500 rounded hover:border-gray-500 hover:border-2 hover:rounded-lg hover:bg-gray-200">{t("profile.button")}</button>
+            <button className="block w-full p-3 mb-4 text-black-500 rounded hover:border-gray-500 hover:border-2 hover:rounded-lg hover:bg-gray-200">{t("profile.button2")}</button>
+            <button className="block w-full p-3 mb-4 text-black-500 rounded hover:border-gray-500 hover:border-2 hover:rounded-lg hover:bg-gray-200">{t("profile.button3")}</button>
+            <button className="block w-full p-3 mb-4 text-black-500 rounded hover:border-gray-500 hover:border-2 hover:rounded-lg hover:bg-gray-200">{t("profile.button4")} </button>
+            <button className="block w-full p-3 mb-4 text-red-600 rounded hover:border-red-500 hover:border-2 hover:rounded-lg hover:bg-red-200" onClick={handleLogout}>{t("profile.button5")}</button>
+          </div>
+        </div>
+      </>
     );
   }
-
-
 };
 
 export default Profile;
