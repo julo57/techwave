4c932f8a38bc5e69a3144b9fba35d2da1aa1ae3e
ada <aniepala1411@gmail.com>
2023-11-27 15:16:02 +0100

    Szybsze ładowanie produktów

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,23 +1,22 @@
-import React, { useContext } from "react";
+
+import React, { useContext, useMemo, useCallback } from "react";
 import { ShopContext } from "../../context/shop-context";
 import { useTranslation } from "react-i18next";
 import useProduct from "./useProduct";
 import { useNavigate } from "react-router-dom";
 
-export const Product = (props) => {
+export const Product = React.memo((props) => {
   const {t} = useTranslation("global");
   const { id, name, price, photo } = props.data;
   const { addToCart, cartItems } = useContext(ShopContext);
-  const { isLoading, error, product } = useProduct("2");
-  console.log("product", {product, isLoading, error})
+  const { isLoading, error, product } = useProduct(id); // Use dynamic id
   const navigate = useNavigate();
 
-  const handleProductClick = (productId) => {
-    console.log("clicked");
-    navigate(`/ProductSite/${productId}`);
-  };
-  
-  const cartItemCount = cartItems[id];
+  const handleProductClick = useCallback(() => {
+    navigate(`/ProductSite/${id}`);
+  }, [id, navigate]);
+
+  const cartItemCount = useMemo(() => cartItems[id], [cartItems, id]);
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -26,17 +25,20 @@ export const Product = (props) => {
   if (error) {
     return <p>Error: {error.message}</p>;
   }
+
   return (
-    <div className="product" onClick={() =>handleProductClick(id)}>
-      <img src={photo} />
+    <div className="product" onClick={handleProductClick}>
+      <img src={photo} alt={name} loading="lazy" />
       <div className="description">
         <p>
           <b>{name}</b>
         </p>
         <p> PLN {price}</p>
       </div>
-  
     </div>
   );
-};
+});
 
+export  default Product;
+
+ 
