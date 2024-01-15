fdfad3eba02758e041e0b3e2aff6c72e286ec5af
julo57 <julian.mikuskiewicz@gmail.com>
2024-01-15 15:50:32 +0100

    Zmiana

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"http://localhost:8000",
+    baseURL:"http://techwave-online-shop.wuaze.com/",
     withCredentials: true,
 })
